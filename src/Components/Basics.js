f742242b320dbc2bb44b359c5a19df0be412c21c
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-11-14 23:31:58 +0530

    use effect

---
+++ src/Components/Basics.js
@@ -0,0 +1,29 @@
+import React,{useState, useEffect} from "react";
+
+
+const Basics = () => {
+ const [gold, setGold] = useState(0);
+ const [silver, setSilver] = useState(0);
+ const [bronze, setBronze] = useState(0);
+
+
+ useEffect(()=>{
+        console.log("Before")
+        document.title = `Gold: ${gold} Silver: ${silver}`
+        console.log("After")
+ }, [gold,silver])
+
+
+    
+
+    return(
+        <div>
+             <h1> Gold: {gold} Silver: {silver} Bronze: {bronze}</h1>
+             <button onClick={() => setGold(gold + 1)}>Gold</button>
+             <button onClick={() => setSilver(silver + 1)}>Silver</button>
+             <button onClick={() => setBronze(bronze + 1)}>Bronze</button>
+        </div>
+    )
+}
+
+export default Basics;
